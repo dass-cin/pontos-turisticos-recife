e858d14dd9a6e6e14336738d1449e69dd0927758
Diego Pessoa <diegopessoa12@gmail.com>
2015-11-24 21:51:27 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+
+e = create_engine('sqlite:///museus.db')
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Museus_Meta(Resource):
+    def get(self):
+        conn = e.connect()
+        query = conn.execute('select * from museus')
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return result
+
+
+api.add_resource(Museus_Meta, '/museus/')
+
+if __name__ == '__main__':
+    app.run()
